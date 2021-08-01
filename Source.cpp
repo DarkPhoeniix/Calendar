7ad6f8b2a37af171da24b8dcdfe10a4e99d45d24
MarynaVovchuk <vovchuk.marina3@gmail.com>
2021-08-01 22:02:41 +0300

    chaned behaviour of deleting and finding elements. changed way of input

--- Source.cpp
+++ Source.cpp
@@ -1,7 +1,10 @@
 
 #include <iostream>
+#include <numeric>
 
+#include "Condition_Parser.h"
 #include "DataBase.h"
+#include "Node.h"
 
 int main()
 {
@@ -10,29 +13,47 @@ int main()
 	std::string command;
 	std::string eventName;
 
-	while(true) {
+	for (std::string line; getline(std::cin, line); ) {
+		std::istringstream iss(line);
 		try {
-			std::cin >> command;
+			iss >> command;
 			if (command == "Add") {
-				std::cin >> date >> eventName;
+				iss >> date >> eventName;
+				while (!iss.eof()) {
+					eventName += iss.get();
+				}
 				dataBase.add(date, eventName);
 			}
 			else if (command == "Del") {
-				std::cin >> date;
-				if (std::cin.get() == ' ') {
-					std::cin >> eventName;
-					(dataBase.remove(date, eventName)) ? 
-						std::cout << "Deleted successfully" << std::endl :
-						std::cout << "Event not found" << std::endl;
-				} else {
-					std::cout << "Deleted " << dataBase.remove(date) << " events" << std::endl;
-				}
+				std::shared_ptr<Node> root = parseCondition(iss);
+				auto pred = [root](const Date& date, const std::string& eventName) 
+					{ 
+						return root->evaluate(date, eventName); 
+					};
+				std::cout << "Deleted " << dataBase.removeIf(pred) << " events" << std::endl;
 			}
 			else if (command == "Find") {
-				std::cin >> date;
-				for (const auto& item : dataBase.find(date)) {
-					std::cout << date << ' ' << item << std::endl;
+				std::shared_ptr<Node> root = parseCondition(iss);
+				auto pred = [root](const Date& date, const std::string& eventName)
+				{
+					return root->evaluate(date, eventName);
+				};
+				auto foundEvents = dataBase.findIf(pred);
+				int num = 0;
+				std::for_each(foundEvents.begin(), 
+							  foundEvents.end(), 
+							  [&num](auto& p) { num += p.second.size(); }
+				);
+				std::cout << "Found " << num << " events" << std::endl;
+				for (const auto& [date, events] : foundEvents) {
+					for (const auto& eventName : events) {
+						std::cout << date << ' ' << eventName << std::endl;
+					}
 				}
+				//std::cin >> date;
+				//for (const auto& item : dataBase.find(date)) {
+				//	std::cout << date << ' ' << item << std::endl;
+				//}
 			}
 			else if (command == "Print") {
 				dataBase.print();
@@ -46,6 +67,7 @@ int main()
 		} catch (std::exception& ex) {
 			std::cout << ex.what() << std::endl;
 		}
+		command = "";
 	}
 
 	return 0;
